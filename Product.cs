910c7d80b791487f4202ebd24e9ecabfac88e55d
christopouloumarianna <50484642+christopouloumarianna@users.noreply.github.com>
2019-05-17 01:18:09 +0300

    Initial commit

---
+++ Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLDBO
+{
+    class Product
+    {
+        public int Id { get; set; }
+
+
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+
+
+        public Product(int id, string name, decimal price, string grosCategory)
+
+        {
+
+            Id = id;
+
+            Name = name;
+
+            Price = price;
+
+            Category = grosCategory;
+
+        }
+
+
+
+        public override string ToString()
+
+        {
+
+            return $"ID : {Id} \t Name : {Name} \t Price : {Price} \t Product Category : {Category}";
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SQLDBO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new Msql();
+
+            //db.M1sql(); // save to json data from server localhost
+
+            db.InsertDB(); 
+
+            Console.ReadLine();
+        }
+    }
+}
